2606fb48da8ca8a77a4e2cf0156b0166fd06a868
angelomarsanz <angelomarsanz@gmail.com>
2015-05-28 21:51:59 -0430

    Update javascriptcontacto.js

--- javascriptcontacto.js
+++ javascriptcontacto.js
@@ -20,20 +20,31 @@ function validacion()
 		micomentario2.style.background='#FFDDDD';
 		}
 	}
+function cambiarrango ()
+	{
+		var salida=document.getElementById("rango");
+		var calculo=miedad2.value-20;
+		if(calculo<20)
+			{
+				calculo=0;
+				miedad2.value=20;
+			}
+		salida.innerHTML=calculo+' a '+miedad2.value;	
+	}	
 function iniciar() 
 	{
 	minombre2=document.getElementById("minombre");
 	micorreo2=document.getElementById("micorreo");
 	micelular2=document.getElementById("micelular");
 	mitelefonofijo2=document.getElementById("mitelefonofijo");
+	miedad2=document.getElementById("miedad");	
 	micomentario2=document.getElementById("micomentario");
 	minombre2.addEventListener("input", validacion, false);
 	micorreo2.addEventListener("input", validacion, false);
 	micelular2.addEventListener("input", validacion, false);
 	mitelefonofijo2.addEventListener("input", validacion, false);
+	miedad2.addEventListener("change", cambiarrango, false);	
 	micomentario2.addEventListener("input", validacion, false);
 	validacion();
 	}
 window.addEventListener("load", iniciar, false);
-
-
